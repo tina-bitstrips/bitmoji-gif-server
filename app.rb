0d6f803f56597f1a50b76b7764e87fb370f27f7e
Tina Holly <tina@bitstrips.com>
2015-12-18 16:22:48 -0500

    ruby version because gifs in node is awful

---
+++ app.rb
@@ -0,0 +1,53 @@
+require 'sinatra/base'
+
+class BitmojiGifServer < Sinatra::Base
+  require 'RMagick'
+  require 'fileutils'
+  require "open-uri"
+  require 'json'
+
+  set :static, true
+  set :public_folder, '/tmp'
+
+  use Rack::Static, :urls => ['/tmp']
+  
+  get '/templates' do
+
+    gifs = []
+
+    tinaAvatarIdForDebugging = '104556134_20-s4-v1';
+    template_url = 'https://da8lb468m8h1w.cloudfront.net/v2/cpanel/%s-%s.png?palette=1';
+
+    data = [
+      [ 7193113, 9462153, 7193113, 9462157 ], # world's crudest waving gif,
+      [ 9449032, 9462306, 9462338, 9462322 ], # basic santa wave v1
+      [ 9462463, 9462473, 9462485, 9462505 ], # good morning
+      [ 9463270, 9463272, 9463273, 9463275, 9463287, 9463289, 9463297, 9463296, 9463299, 9463300 ], # rain
+      [ 9463357, 9463361, 9463365, 9463368, 9463370, 9463374 ] # confetti
+    ]
+
+    data.each_with_index do |set, index|
+      directory = "tmp/#{index}"
+      FileUtils::mkdir_p directory
+
+      images_to_gif = set.map do |template_id|
+        template = sprintf template_url, template_id, tinaAvatarIdForDebugging
+
+        File.open("#{directory}/#{template_id}.png", 'wb') do |fo|
+          fo.write open(template).read 
+        end
+      end
+
+      # make the GIFS
+      animation = Magick::ImageList.new(*Dir["#{directory}/*.png"])
+      animation.delay = 10
+      animation.write("#{directory}/animated.gif")
+      
+      gifs.push("/#{directory}/animated.gif")
+    end
+
+    return { "data" => gifs }.to_json
+
+  end
+
+end
---
+++ gif_maker.rb
@@ -0,0 +1,17 @@
+
+
+include Magick
+
+class GifMaker
+  def initialize()
+    @gifs = []
+  end
+
+  def get_gifs
+    
+  end
+
+end
+
+
+
